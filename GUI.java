d1d3dea0d42a9b10a597fb77c8cecb235bcd80a3
Andrei Valdez <150660845+ReiZedlav@users.noreply.github.com>
2024-11-11 22:23:24 +0800

    Update GUI.java
    Resized - Optimize - New Designer Feature

--- GUI.java
+++ GUI.java
@@ -8,27 +8,48 @@ class GUI {
     }
 }
 
-class Designs {
-    private String flagLeft = "emptyCountry.jpg";
-    private String flagRight = "emptyCountry.jpg";
+class Configurations{
+    //config
+    protected static String[] currencies = {
+            "USD", "EUR", "GBP", "JPY", "AUD", "CAD", "CHF", "CNY", "INR", "MXN",
+            "BRL", "RUB", "ZAR", "KRW", "SGD", "NZD", "SEK", "NOK", "DKK", "TRY"
+        };
 
-    private String countryLeft = "Country";
-    private String countryRight = "Country";
+    protected static String flagLeft = "emptyCountry.jpg";
+    protected static String flagRight = "emptyCountry.jpg";
 
-    private String countryCodeLeft = "XXX";
-    private String countryCodeRight = "XXX";
+    protected static String countryLeft = "Country";
+    protected static String countryRight = "Country";
 
-    private int convertedCurrency = 0;
-    private String strConvertedCurrency = String.valueOf(convertedCurrency); // Converts convertedCurrency to String for GUI.
+    protected static String countryCodeLeft = "XXX";
+    protected static String countryCodeRight = "XXX";
+
+    protected static int convertedCurrency = 0;
+    protected static String strConvertedCurrency = String.valueOf(convertedCurrency); // Converts convertedCurrency to String for GUI.
+
+    //REPETITIVE GUI COORDS
+
+    protected static int CountryFlagXAxisCoords = 50;
+    protected static int CountryFlagYAxisCoords = 20;
+
+    protected static int CountryTextXAxisCoords = 85; //Country Text X axis
+    protected static int CountryTextYAxisCoords = 130;
+
+    protected static int CountryCodeXAxisCoords = 41;
+    protected static int CountryCodeYAxisCoords = 170;
+
+    protected static int CountryDropdownXAxisCoords = 15;
+    protected static int CountryDropdownYAxisCoords = 270;
+}
+
+
+
+
+class Designs extends Configurations {
 
     public Designs() {}
 
     public void Menu() {
-
-        String[] currencies = {
-            "USD", "EUR", "GBP", "JPY", "AUD", "CAD", "CHF", "CNY", "INR", "MXN",
-            "BRL", "RUB", "ZAR", "KRW", "SGD", "NZD", "SEK", "NOK", "DKK", "TRY"
-        };
 
         // Mother of all GUI
         JFrame frame = new JFrame();
@@ -40,7 +61,7 @@ class Designs {
         ImageIcon swapLogo = new ImageIcon("swap.jpg");
 
         // Swap button configurations
-        swap.setBounds(50, 35, 50, 50);
+        swap.setBounds(75, 35, 50, 50);
         swap.setIcon(swapLogo);
 
         submit.setBounds(120, 125, 200, 35); // Submit Button size and coordinates.
@@ -50,30 +71,30 @@ class Designs {
 
         JPanel left = new JPanel();
         left.setBackground(Color.red);
-        left.setBounds(0, 0, 150, 300);
+        left.setBounds(0, 0, 200, 300);
 
         JLabel leftDesignFlag = new JLabel();
         ImageIcon inputCountry = new ImageIcon(flagLeft); // Left flag design
 
         leftDesignFlag.setIcon(inputCountry);
         leftDesignFlag.setText(countryLeft);
-        leftDesignFlag.setBounds(25, 20, 100, 100);
+        leftDesignFlag.setBounds(CountryFlagXAxisCoords, CountryFlagYAxisCoords, 100, 100);
 
         JLabel leftDesignCountryName = new JLabel();
         leftDesignCountryName.setText(countryLeft);
-        leftDesignCountryName.setBounds(45, 130, 200, 30);
+        leftDesignCountryName.setBounds(CountryTextXAxisCoords, CountryTextYAxisCoords, 200, 30);
         leftDesignCountryName.setFont(new Font("Arial", Font.PLAIN, 18));
 
         JLabel leftDesignCountryCode = new JLabel();
         leftDesignCountryCode.setText(countryCodeLeft);
-        leftDesignCountryCode.setBounds(41, 170, 200, 30);
+        leftDesignCountryCode.setBounds(CountryCodeXAxisCoords, CountryCodeYAxisCoords, 200, 30);
         leftDesignCountryCode.setFont(new Font("Arial", Font.PLAIN, 35));
 
         JTextField leftDesignInputAmount = new JTextField();
         leftDesignInputAmount.setBounds(15, 220, 120, 40);
 
         JComboBox<String> currencyComboBox = new JComboBox<>(currencies);
-        currencyComboBox.setBounds(15, 270, 120, 30);  //COMBO BOX left and right
+        currencyComboBox.setBounds(CountryDropdownXAxisCoords, CountryDropdownYAxisCoords, 120, 30);  //COMBO BOX left and right
         
 
         left.setLayout(null);
@@ -88,7 +109,7 @@ class Designs {
 
         JPanel middle = new JPanel();
         middle.setBackground(Color.green);
-        middle.setBounds(150, 0, 150, 300);
+        middle.setBounds(200, 0, 200, 300);
         middle.setLayout(null);
 
         middle.add(swap);
@@ -97,22 +118,22 @@ class Designs {
 
         JPanel right = new JPanel();
         right.setBackground(Color.blue);
-        right.setBounds(300, 0, 150, 300);
+        right.setBounds(400, 0, 200, 300);
 
         JLabel rightDesignFlag = new JLabel();
         ImageIcon outputCountry = new ImageIcon(flagRight); // Right flag design
 
-        rightDesignFlag.setBounds(25, 20, 100, 100);
+        rightDesignFlag.setBounds(CountryFlagXAxisCoords, CountryFlagYAxisCoords, 100, 100);
         rightDesignFlag.setIcon(outputCountry);
 
         JLabel rightCountryDesignName = new JLabel();
         rightCountryDesignName.setText(countryRight);
-        rightCountryDesignName.setBounds(45, 130, 200, 30);
+        rightCountryDesignName.setBounds(CountryTextXAxisCoords, CountryTextYAxisCoords, 200, 30);
         rightCountryDesignName.setFont(new Font("Arial", Font.PLAIN, 18));
 
         JLabel rightDesignCountryCode = new JLabel();
         rightDesignCountryCode.setText(countryCodeLeft);
-        rightDesignCountryCode.setBounds(41, 170, 200, 30);
+        rightDesignCountryCode.setBounds(CountryCodeXAxisCoords, CountryCodeYAxisCoords, 200, 30);
         rightDesignCountryCode.setFont(new Font("Arial", Font.PLAIN, 35));
 
         JLabel rightDesignAmountNum = new JLabel();
@@ -126,7 +147,7 @@ class Designs {
         rightDesignAmountText.setFont(new Font("Arial", Font.PLAIN, 15));
 
         JComboBox<String> currencyComboBox2 = new JComboBox<>(currencies);
-        currencyComboBox2.setBounds(15, 270, 120, 30); 
+        currencyComboBox2.setBounds(CountryDropdownXAxisCoords, CountryDropdownYAxisCoords, 120, 30); 
 
         right.setLayout(null);
         right.add(rightDesignFlag);
@@ -142,14 +163,14 @@ class Designs {
 
         JPanel bottom = new JPanel();
         bottom.setBackground(Color.yellow);
-        bottom.setBounds(0, 213, 450, 200);
+        bottom.setBounds(0, 213, 600, 200);
         bottom.setLayout(null);
         bottom.add(submit); // This button not showing up on JPanel
 
         //----------------------------------------------------------//
 
         frame.setVisible(true);
-        frame.setSize(450, 450);
+        frame.setSize(600,450);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
 
