4fbdbdc19a0b6e2e593e427ce9164fd90c68a7c1
Andrei Valdez <150660845+ReiZedlav@users.noreply.github.com>
2024-11-20 15:23:09 +0800

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -2,6 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class GUI {
 
         ////////////////////////////////////////////////
@@ -105,14 +109,36 @@ class Designs extends Configurations {
         swap.setBounds(75, 35, 50, 50);
         swap.setIcon(swapLogo);
 
-        submit.setBounds(200, 125, 200, 35);
+        submit.setBounds(200, 35, 200, 35);
         submit.setText("Submit");
+
+        //-------------------------------------------------------------------//
+
+        JPanel topIntro = new JPanel();
+        topIntro.setBackground(Color.orange);
+        topIntro.setBounds(0,0,600,63);
+
+        JLabel topIntroText = new JLabel();
+
+        topIntroText.setText("CSCC20 FINAL PROJECT");
+
+        topIntroText.setBounds(50, 100,300,50);
+        
+        topIntro.add(topIntroText);
+
+        
+
+        //-------------------------------------------------------------------//
+
+        JPanel rightHotBar = new JPanel();
+        rightHotBar.setBackground(Color.cyan);
+        rightHotBar.setBounds(600,0,36,463);
 
         //-----------------------------------------------------------------//
 
         JPanel left = new JPanel();
         left.setBackground(Color.red);
-        left.setBounds(0, 0, 200, 300);
+        left.setBounds(0, 63, 200, 300);
 
         JLabel leftDesignFlag = new JLabel();
         ImageIcon inputCountry = new ImageIcon(flagLeft);
@@ -150,7 +176,7 @@ class Designs extends Configurations {
 
         JPanel middle = new JPanel();
         middle.setBackground(Color.green);
-        middle.setBounds(200, 0, 200, 300);
+        middle.setBounds(200, 63, 200, 300);
         middle.setLayout(null);
 
         middle.add(swap);
@@ -159,7 +185,7 @@ class Designs extends Configurations {
 
         JPanel right = new JPanel();
         right.setBackground(Color.blue);
-        right.setBounds(400, 0, 200, 300);
+        right.setBounds(400, 63, 200, 300);
 
         JLabel rightDesignFlag = new JLabel();
         ImageIcon outputCountry = new ImageIcon(flagRight);
@@ -253,14 +279,17 @@ class Designs extends Configurations {
 
         JPanel bottom = new JPanel();
         bottom.setBackground(Color.yellow);
-        bottom.setBounds(0, 213, 600, 200);
+        bottom.setBounds(0, 363, 600, 100);
         bottom.setLayout(null);
         bottom.add(submit);
 
         frame.setVisible(true);
-        frame.setSize(600,450);
+        frame.setSize(650,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
+
+        frame.add(topIntro);
+        frame.add(rightHotBar);
 
         frame.add(left);
         frame.add(middle);
@@ -279,3 +308,80 @@ class Designs extends Configurations {
             }});
     }
 }
+
+class Currency { //class that serves as a constructor for currencies
+    //attributes
+    private String currencycode; //access modifier
+    private double currencyrate;
+    private String currencyname;
+
+    //methods
+    public Currency(String currencycode, double currencyrate, String currencyname){ //constructors
+        this.currencycode = currencycode; //initializing all attributes
+        this.currencyrate = currencyrate;
+        this.currencyname = currencyname;
+    }
+
+    public String getCode() { return currencycode;} //data hiding, getters 
+    public void setCode(String currencycode) { this.currencycode = currencycode;}//setters
+    public double getRate() { return currencyrate;}
+    public void setRate(double currencyrate) { this.currencyrate = currencyrate;}
+    public String getName() { return currencyname;}
+    public void setName(String currencyname) {this.currencyname = currencyname; }
+}
+
+class CurrencyData { //keeps all the currency information
+    //attributes
+    private HashMap<String, Currency> currencydata = new HashMap<>();
+    Currency currency;
+    //methods
+    public CurrencyData() {
+        currencydata.put("USD", new Currency ("USD", 1.0, "United States Dollar"));
+        currencydata.put("PHP", new Currency ("PHP", 56.64, "Philippine Peso"));
+        currencydata.put("CNY", new Currency ("CNY", 7.02, "Chinese Yuan"));
+        currencydata.put("EUR", new Currency ("EUR", 0.91, "European Euro"));
+        currencydata.put("AED", new Currency  ("AED", 3.67, "United Arab Emirates Dirham"));
+        currencydata.put("KRW", new Currency ("KRW",  1346.87, "South Korean Won"));
+        currencydata.put("JPY", new Currency("JPY", 148.72, "Japanese Yen"));
+        currencydata.put("INR", new Currency("INR", 84.03, "Indian Rupee"));
+        currencydata.put("CAD", new Currency("CAD", 1.36, "Canadian Dollar"));
+        currencydata.put("THB", new Currency ("THB", 33.28, "Thai Baht"));
+        currencydata.put("MXN", new Currency ("MXN", 20.14, "Mexican Peso"));
+        currencydata.put("VND", new Currency ("VND", 25289.85, "Vietnamese Dong"));
+        currencydata.put("CHF", new Currency ("CHF", 0.88, "Swiss Franc"));
+        currencydata.put("SGD", new Currency ("SGD", 1.33, "Singaporean Dollar"));
+        currencydata.put("SAR", new Currency ("SAR", 3.76, "Saudi Riyal"));
+        currencydata.put("PLN", new Currency ("PLN", 4.05, "Polish złoty"));
+        currencydata.put("RON", new Currency ("RON", 4.65, "Romanian Leu"));
+        currencydata.put("MYR", new Currency ("MYR", 4.41, "Malaysian Ringgit"));
+        currencydata.put("RUB", new Currency ("RUB", 98.00, "Russian Ruble"));
+        currencydata.put("AUD", new Currency ("AUD", 1.52, "Australian Dollar"));
+    }
+    public double getCDataRate(String currencyCode) { //needed for converter
+        currency = currencydata.get(currencyCode);
+        return (currency != null) ? currency.getRate() : 0.0;
+    }
+    public Map<String, Currency> getCurrencies() {return new HashMap<>(currencydata);} //needed for currency display
+}
+
+// Converting Abstract class
+abstract class converter {
+    public double sourceRate;
+    public double targetRate;
+    public double amount;
+    public abstract double convert(double amount, String source, String target, CurrencyData rates);}
+
+class convert extends converter { //initialized subclass
+    public double sourceRate;
+    public double targetRate;
+    public double amount;
+    public double convert(double amount, String source, String target, CurrencyData rates) { //constructor initialized
+        //attributes
+        sourceRate = rates.getCDataRate(source); //get source
+        targetRate = rates.getCDataRate(target); //get target
+        //methods (if is not a method)
+        if (sourceRate == 0.0 || targetRate == 0.0) { throw new IllegalArgumentException("Invalid currency code");} //error catching
+        return (amount / sourceRate) * targetRate;  //converting formula
+    }
+}  
+
